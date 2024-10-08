c3695909cc75e5e2d19bc08391b4a0a39989aacc
Michael Liu <bbz975749@gmail.com>
2024-10-08 10:54:38 +0800

    10/8第二次作业

---
+++ Hw2/WpfApp/FireAlarm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace WpfApp
+{
+    public class FireAlarmEventArgs : EventArgs
+    {
+        public string Location { get; set; }
+        public int Severity { get; set; }
+
+        public FireAlarmEventArgs(string location, int severity)
+        {
+            Location = location;
+            Severity = severity;
+        }
+    }
+
+    public class FireAlarm
+    {
+        public delegate void FireAlarmEventHandler(object sender, FireAlarmEventArgs e);
+        public event FireAlarmEventHandler FireEvent;
+
+        public void TriggerFireAlarm(string location, int severity)
+        {
+            FireEvent?.Invoke(this, new FireAlarmEventArgs(location, severity));
+        }
+    }
+
+    public class FireDepartment
+    {
+        private MainWindow _mainWindow;
+
+        public FireDepartment(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+        public void OnFireAlarmRaised(object sender, FireAlarmEventArgs e)
+        {
+            _mainWindow.AppendTextToTextBox($"Fire in {e.Location} with severity {e.Severity}. Fire Department responding.");
+        }
+    }
+
+    public class SecurityTeam
+    {
+        private MainWindow _mainWindow;
+
+        public SecurityTeam(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        public void OnFireAlarmRaised(object sender, FireAlarmEventArgs e)
+        {
+            _mainWindow.AppendTextToTextBox($"Fire in {e.Location} with severity {e.Severity}. Security Team responding.");
+        }
+    }
+
+
+}
---
+++ Hw2/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿// MainWindow.xaml.cs
+using System;
+using System.Windows;
+
+namespace WpfApp
+{
+
+    public partial class MainWindow : Window
+    {
+        private FireAlarm fireAlarm;
+        private FireDepartment fireDepartment;
+        private SecurityTeam securityTeam;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            fireAlarm = new FireAlarm();
+            fireDepartment = new FireDepartment(this);
+            securityTeam = new SecurityTeam(this);
+        }
+        public void AppendTextToTextBox(string message)
+        {
+            OutputTextBox.AppendText(message + "\n");
+        }
+
+        private void Bind1()
+        {
+            fireAlarm.FireEvent += fireDepartment.OnFireAlarmRaised;
+        }
+
+        private void Bind2()
+        {
+            fireAlarm.FireEvent += securityTeam.OnFireAlarmRaised;
+        }
+
+        private void Unbind1()
+        {
+            fireAlarm.FireEvent -= fireDepartment.OnFireAlarmRaised;
+        }
+
+        private void Unbind2()
+        {
+            fireAlarm.FireEvent -= securityTeam.OnFireAlarmRaised;
+        }
+
+        private void BtnTrigger_Click(object sender, RoutedEventArgs e)
+        {
+            fireAlarm.TriggerFireAlarm("Main Building", 5);
+        }
+
+        private void Bind1_Click(object sender, RoutedEventArgs e)
+        {
+            Bind1();
+            OutputTextBox.AppendText("FireDepartment 绑定成功\n");
+        }
+
+        private void Bind2_Click(object sender, RoutedEventArgs e)
+        {
+            Bind2();
+            OutputTextBox.AppendText("SecurityTeam 绑定成功\n");
+        }
+
+        private void Unbind1_Click(object sender, RoutedEventArgs e)
+        {
+            Unbind1();
+            OutputTextBox.AppendText("FireDepartment 解绑成功\n");
+        }
+
+        private void Unbind2_Click(object sender, RoutedEventArgs e)
+        {
+            Unbind2();
+            OutputTextBox.AppendText("SecurityTeam 解绑成功\n");
+        }
+    }
+}

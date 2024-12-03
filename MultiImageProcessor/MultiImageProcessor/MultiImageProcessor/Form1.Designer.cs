656a036f7dff27f77e54838f0f1dd953dffc9bbf
Michael Liu <bbz975749@gmail.com>
2024-12-03 14:18:49 +0800

    12/3
    初步框架

---
+++ MultiImageProcessor/MultiImageProcessor/MultiImageProcessor/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace MultiImageProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "文件列表:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(192, 247);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(537, 256);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(491, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "添加图像";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(754, 247);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 51);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "删除所选文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(754, 399);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(220, 68);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "查看选中文件的处理结果";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "灰度",
+            "放大至200%",
+            "缩小至50%",
+            "顺时针旋转90°",
+            "逆时针旋转90°"});
+            this.comboBox1.Location = new System.Drawing.Point(192, 526);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 26);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(377, 519);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 38);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "开始处理";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(533, 523);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 31);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "取消处理";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1012, 247);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(643, 458);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1684, 1021);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ MultiImageProcessor/MultiImageProcessor/MultiImageProcessor/Form1.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace MultiImageProcessor
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> imageQueue = new Queue<string>();
+        bool isCancelProcessing = false;
+        Queue<ProgressInfo> progressQueue = new Queue<ProgressInfo>();
+
+        private Bitmap ProcessImageGrayScale(string filePath)//灰度处理函数
+        {
+            Bitmap original = new Bitmap(filePath);
+            Bitmap grayScale = new Bitmap(original.Width, original.Height);
+            for (int x = 0; x < original.Width; x++)
+            {
+                for (int y = 0; y < original.Height; y++)
+                {
+                    Color originalColor = original.GetPixel(x, y);
+                    int gray = (int)(originalColor.R * 0.299 + originalColor.G * 0.587 + originalColor.B * 0.114);
+                    grayScale.SetPixel(x, y, Color.FromArgb(gray, gray, gray));
+                }
+            }
+            return grayScale;
+        }
+
+        private Bitmap ProcessImageEnlarge(string filePath, double factor)//放大200%
+        {
+            Bitmap original = new Bitmap(filePath);
+            int newWidth = (int)(original.Width * factor);
+            int newHeight = (int)(original.Height * factor);
+            Bitmap enlarged = new Bitmap(newWidth, newHeight);
+            using (Graphics g = Graphics.FromImage(enlarged))
+            {
+                g.DrawImage(original, 0, 0, newWidth, newHeight);
+            }
+            return enlarged;
+        }
+
+        private Bitmap ProcessImageReduce(string filePath, float factor)//缩小50%
+        {
+            Bitmap original = new Bitmap(filePath);
+            int newWidth = (int)(original.Width * factor);
+            int newHeight = (int)(original.Height * factor);
+            Bitmap reduced = new Bitmap(newWidth, newHeight);
+            using (Graphics g = Graphics.FromImage(reduced))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(original, 0, 0, newWidth, newHeight);
+            }
+            return reduced;
+        }
+
+        private Bitmap ProcessImageRotateClockwise(string filePath)//顺时针90度
+        {
+            Bitmap original = new Bitmap(filePath);
+            Bitmap rotated = new Bitmap(original.Height, original.Width);
+            for (int x = 0; x < original.Width; x++)
+            {
+                for (int y = 0; y < original.Height; y++)
+                {
+                    Color color = original.GetPixel(x, y);
+                    rotated.SetPixel(original.Height - 1 - y, x, color);
+                }
+            }
+            return rotated;
+        }
+
+        private Bitmap ProcessImageRotateCounterclockwise(string filePath)//逆时针90度
+        {
+            Bitmap original = new Bitmap(filePath);
+            Bitmap rotated = new Bitmap(original.Height, original.Width);
+            for (int x = 0; x < original.Width; x++)
+            {
+                for (int y = 0; y < original.Height; y++)
+                {
+                    Color color = original.GetPixel(x, y);
+                    rotated.SetPixel(y, original.Width - 1 - x, color);
+                }
+            }
+            return rotated;
+        }
+
+        private void ProcessImageInThread(string filePath, string processingOption)
+        {
+            Bitmap processedImage = null;
+            switch (processingOption)
+            {
+                case "灰度":
+                    processedImage = ProcessImageGrayScale(filePath);
+                    break;
+                case "放大至200%":
+                    processedImage = ProcessImageEnlarge(filePath, 2);
+                    break;
+                case "缩小至50%":
+                    processedImage = ProcessImageReduce(filePath, 0.5f);
+                    break;
+                case "顺时针旋转90°":
+                    processedImage = ProcessImageRotateClockwise(filePath);
+                    break;
+                case "逆时针旋转90°":
+                    processedImage = ProcessImageRotateCounterclockwise(filePath);
+                    break;
+            }
+            // 这里后续可以添加代码将处理后的图像保存或者更新到界面展示等操作
+            if (processedImage != null)
+            {
+                string fileNameWithoutExtension = Path.ChangeExtension(filePath, null);
+                string fileExtension = Path.GetExtension(filePath);
+                string processedFilePath = "";
+
+                // 根据处理选项确定保存路径
+                switch (processingOption)
+                {
+                    case "灰度":
+                        processedFilePath = fileNameWithoutExtension + "_gray" + fileExtension;
+                        break;
+                    case "放大至200%":
+                        processedFilePath = fileNameWithoutExtension + "_enlarged" + fileExtension;
+                        break;
+                    case "缩小至50%":
+                        processedFilePath = fileNameWithoutExtension + "_reduced" + fileExtension;
+                        break;
+                    case "顺时针旋转90°":
+                        processedFilePath = fileNameWithoutExtension + "_rotated_clockwise" + fileExtension;
+                        break;
+                    case "逆时针旋转90°":
+                        processedFilePath = fileNameWithoutExtension + "_rotated_counterclockwise" + fileExtension;
+                        break;
+                }
+
+                // 保存处理后的图像
+                if (!File.Exists(processedFilePath))
+                {
+                    processedImage.Save(processedFilePath);
+                }
+
+            }
+            // 处理完成后，找到对应的ProgressInfo对象并更新状态
+            foreach (ProgressInfo progress in progressQueue)
+            {
+                if (progress.FileName == filePath && progress.Status == "处理中")
+                {
+                    progress.Status = "已处理";
+                    // 更新ListBox中的状态
+                    int index = listBox1.Items.IndexOf(filePath + " [处理中]");
+                    if (index != -1)
+                    {
+                        listBox1.Items[index] = filePath + " [已处理]";
+                    }
+                    break;
+                }
+            }
+        }
+
+        //private void UpdateProgress()
+        //{
+        //    //if (progressQueue.Count > 0)
+        //    //{
+        //    //    ProgressInfo info = progressQueue.Dequeue();
+        //    //    // 根据info中的文件名找到界面文件列表里对应的项，更新其显示的处理状态
+        //    //}
+        //    if (progressQueue.Count > 0)
+        //    {
+        //        ProgressInfo progress = progressQueue.Dequeue();
+
+        //        // 在ListBox中更新文件状态
+        //        int listBoxIndex = listBox1.Items.IndexOf(progress.FileName + " [待处理]");
+        //        if (listBoxIndex != -1)
+        //        {
+        //            listBox1.Items[listBoxIndex] = progress.FileName + $" [ {progress.Status} ]";
+        //        }
+        //    }
+        //}
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            //System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            //timer.Interval = 100;
+            //timer.Tick += (s, args) => UpdateProgress();
+            //timer.Start();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "图像文件|*.jpg;*.png;*.jpeg";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                imageQueue.Enqueue(filePath);
+                listBox1.Items.Add(filePath + " [待处理]");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            while (listBox1.SelectedItems.Count > 0)
+            {
+                string selectedItem = listBox1.SelectedItem.ToString();
+                //string filePath = selectedItem.Substring(0, selectedItem.IndexOf(" [待处理]"));
+                int startIndex = selectedItem.LastIndexOf(" [");
+                string filePath = selectedItem.Substring(0, startIndex);
+                imageQueue = new Queue<string>(imageQueue.Where(x => x != filePath));
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            isCancelProcessing = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //string processingOption = comboBox1.SelectedItem.ToString();
+            //foreach (string filePath in imageQueue)
+            //{
+            //   string tempOption = processingOption;
+            //   Thread thread = new Thread(() => ProcessImageInThread(filePath, tempOption));
+            //   thread.Start();
+            //}
+            string processingOption = comboBox1.SelectedItem.ToString();
+            foreach (string filePath in imageQueue)
+            {
+                string tempOption = processingOption;
+                // 创建ProgressInfo对象并添加到队列
+                ProgressInfo progress = new ProgressInfo
+                {
+                    FileName = filePath,
+                    Status = "处理中"
+                };
+                progressQueue.Enqueue(progress);
+                // 更新ListBox中的状态
+                int index = listBox1.Items.IndexOf(filePath + " [待处理]");
+                if (index != -1)
+                {
+                    listBox1.Items[index] = filePath + " [处理中]";
+                }
+                Thread thread = new Thread(() => ProcessImageInThread(filePath, tempOption));
+                thread.Start();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                string selectedItem = listBox1.SelectedItem.ToString();
+                int startIndex = selectedItem.LastIndexOf(" [");
+                string originalFilePath = selectedItem.Substring(0, startIndex);
+                string fileNameWithoutExtension = Path.ChangeExtension(originalFilePath, null);
+                string fileExtension = Path.GetExtension(originalFilePath);
+
+                string selectedOption = comboBox1.SelectedItem.ToString();
+                string processedFilePath = "";
+
+                // 根据选择的不同处理选项，确定处理后图像的文件名
+                switch (selectedOption)
+                {
+                    case "灰度":
+                        processedFilePath = fileNameWithoutExtension + "_gray" + fileExtension;
+                        break;
+                    case "放大至200%":
+                        processedFilePath = fileNameWithoutExtension + "_enlarged" + fileExtension;
+                        break;
+                    case "缩小至50%":
+                        processedFilePath = fileNameWithoutExtension + "_reduced" + fileExtension;
+                        break;
+                    case "顺时针旋转90°":
+                        processedFilePath = fileNameWithoutExtension + "_rotated_clockwise" + fileExtension;
+                        break;
+                    case "逆时针旋转90°":
+                        processedFilePath = fileNameWithoutExtension + "_rotated_counterclockwise" + fileExtension;
+                        break;
+                }
+
+                if (File.Exists(processedFilePath))
+                {
+                    try
+                    {
+                        Bitmap processedImage = new Bitmap(processedFilePath);
+                        pictureBox1.Image = processedImage;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"加载处理后图像时出错: {ex.Message}");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("未找到对应的处理后图像文件，请先确保已成功处理该图像。");
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+class ProgressInfo//声明用于存储处理进度信息的队列（比如包含文件名、处理状态等内容）
+{
+    public string FileName;
+    public string Status;
+}
+
+
